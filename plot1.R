65faee589c9dd817dc0c0aae19e7319fb9aac314
Robert Laing <robert.laing@rocketmail.com>
2015-03-04 18:43:23 +0200

    Changed red to #ff2500 found with Gimp color picker

--- plot1.R
+++ plot1.R
@@ -1,13 +1,3 @@
-# "Date"
-# "Time"
-# "Global_active_power"  
-# "Global_reactive_power" 
-# "Voltage"
-# "Global_intensity"     
-# "Sub_metering_1"
-# "Sub_metering_2"        
-# "Sub_metering_3"
-
 power_data <- read.table("household_power_consumption.txt", header = TRUE, sep = ";", na.strings = "?")
 power_data$Date <- strptime(power_data$Date, "%d/%m/%Y")
 day1 <- as.POSIXlt("2007-02-01")
@@ -17,6 +7,6 @@ power_data <- subset(power_data, power_data$Date == day1 | power_data$Date == da
 png(file = "plot1.png", width = 480, height = 480, units = "px", bg = "transparent")
 hist(power_data$Global_active_power,
      main = "Global Active Power",
-     col = "red",
+     col = "#ff2500",
      xlab = "Global Active Power (kilowatts)")
 dev.off()
